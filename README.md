1fae96a30c951639b681f45ae19db733388d9705
liviayurike <kliviayurike@gmail.com>
2020-09-23 18:25:31 +0700

    InstrumentTest - Testing Kalkulator

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # Elegant_Calculator
+
+Kelas TI 3F - Breakout Room 1
+
+## Anggota Kelompok
+
+1. Livia Yurike Khuril Maula - 1841720025
+2. Muhammad Fachry Najib - 1841720011
+
 Features - 
 1) Responsive UI.
 2) It can perform all basic operations.
--- app/src/androidTest/java/com/example/new_sample/TesKlikButton.java
+++ app/src/androidTest/java/com/example/new_sample/TesKlikButton.java
@@ -21,69 +21,76 @@ public class TesKlikButton {
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void tesKlikButtonSatu() {
+    public void tesKlikButton() {
         //cari id button1 terus di klik
         onView(withId(R.id.button1)).perform(click());
-        onView(withId(R.id.button2)).perform(click());
-        onView(withId(R.id.button3)).perform(click());
-        onView(withId(R.id.button4)).perform(click());
-        onView(withId(R.id.button5)).perform(click());
-        onView(withId(R.id.button6)).perform(click());
-        onView(withId(R.id.button7)).perform(click());
-        onView(withId(R.id.button8)).perform(click());
-        onView(withId(R.id.button9)).perform(click());
-        onView(withId(R.id.button0)).perform(click());
-        onView(withId(R.id.input)).check(matches(withText("1234567890")));
+        onView(withId(R.id.button2)).perform(click()); //cari id button2 terus di klik
+        onView(withId(R.id.button3)).perform(click()); //cari id button3 terus di klik
+        onView(withId(R.id.button4)).perform(click()); //cari id button4 terus di klik
+        onView(withId(R.id.button5)).perform(click()); //cari id button5 terus di klik
+        onView(withId(R.id.button6)).perform(click()); //cari id button6 terus di klik
+        onView(withId(R.id.button7)).perform(click()); //cari id button7 terus di klik
+        onView(withId(R.id.button8)).perform(click()); //cari id button8 terus di klik
+        onView(withId(R.id.button9)).perform(click()); //cari id button9 terus di klik
+        onView(withId(R.id.button0)).perform(click()); //cari id button0 terus di klik
+        onView(withId(R.id.input)).check(matches(withText("1234567890"))); //menampilkan input button 1234567890
     }
 
     @Test
+    //test untuk perhitungan perkalian
     public void tesPerkalian() {
-        onView(withId(R.id.button1)).perform(click());
-        onView(withId(R.id.button_multi)).perform(click());
-        onView(withId(R.id.button2)).perform(click());
-        onView(withId(R.id.button_equal)).perform(click());
-        onView(withId(R.id.output)).check(matches(withText("2")));
+        onView(withId(R.id.button1)).perform(click()); //cari id button1 terus di klik
+        onView(withId(R.id.button_multi)).perform(click()); //cari id button_multi terus di klik
+        onView(withId(R.id.button2)).perform(click()); //cari id button2 terus di klik
+        onView(withId(R.id.button_equal)).perform(click()); //cari id button_equal terus di klik
+        onView(withId(R.id.output)).check(matches(withText("2"))); //menampilkan hasil perkalian 1 * 2 = 2
     }
 
     @Test
+    //test untuk perhitungan penjumlahan
     public void tesPenjumlahan() {
-        onView(withId(R.id.button1)).perform(click());
-        onView(withId(R.id.button_add)).perform(click());
-        onView(withId(R.id.button2)).perform(click());
-        onView(withId(R.id.button_equal)).perform(click());
-        onView(withId(R.id.output)).check(matches(withText("3")));
+        onView(withId(R.id.button1)).perform(click()); //cari id button1 terus di klik
+        onView(withId(R.id.button_add)).perform(click()); //cari id button_add terus di klik
+        onView(withId(R.id.button2)).perform(click()); //cari id button2 terus di klik
+        onView(withId(R.id.button_equal)).perform(click()); //cari id button_equal terus di klik
+        onView(withId(R.id.output)).check(matches(withText("3"))); //menampilkan hasil penjumlahan 1 + 2 = 3
 
-        //Error apabile penjumlahan setelah di equals lalu di tambahkan lagi
+        //Error apabila penjumlahan setelah di equals lalu di tambahkan lagi
 //        onView(withId(R.id.button_add)).perform(click());
 //        onView(withId(R.id.button1)).perform(click());
 //        onView(withId(R.id.button_add)).perform(click());
 //        onView(withId(R.id.button2)).perform(click());
 //        onView(withId(R.id.output)).check(matches(withText("5")));
     }
+
     @Test
+    //test untuk perhitungan pembagian
     public void tesPembagian() {
-        onView(withId(R.id.button9)).perform(click());
-        onView(withId(R.id.button0)).perform(click());
-        onView(withId(R.id.button_divide)).perform(click());
-        onView(withId(R.id.button2)).perform(click());
-        onView(withId(R.id.button_equal)).perform(click());
-        onView(withId(R.id.output)).check(matches(withText("45")));
-    }
-    @Test
-    public void tesPengurangan(){
-        onView(withId(R.id.button5)).perform(click());
-        onView(withId(R.id.button_sub)).perform(click());
-        onView(withId(R.id.button2)).perform(click());
-        onView(withId(R.id.button_equal)).perform(click());
-        onView(withId(R.id.output)).check(matches(withText("3")));
+        onView(withId(R.id.button9)).perform(click()); //cari id button9 terus di klik
+        onView(withId(R.id.button0)).perform(click()); //cari id button0 terus di klik
+        onView(withId(R.id.button_divide)).perform(click()); //cari id button_divide terus di klik
+        onView(withId(R.id.button2)).perform(click()); //cari id button2 terus di klik
+        onView(withId(R.id.button_equal)).perform(click()); //cari id button_equal terus di klik
+        onView(withId(R.id.output)).check(matches(withText("45"))); //menampilkan hasil pembagian 90 + 2 = 45
     }
 
     @Test
+    //test untuk perhitungan pengurangan
+    public void tesPengurangan(){
+        onView(withId(R.id.button5)).perform(click()); //cari id button5 terus di klik
+        onView(withId(R.id.button_sub)).perform(click()); //cari id button_sub terus di klik
+        onView(withId(R.id.button2)).perform(click()); //cari id button2 terus di klik
+        onView(withId(R.id.button_equal)).perform(click()); //cari id button_equal terus di klik
+        onView(withId(R.id.output)).check(matches(withText("3"))); //menampilkan hasil pengurangan 5 + 2 = 3
+    }
+
+    @Test
+    //test untuk perhitungan persen
     public void tesPersen(){
-        onView(withId(R.id.button1)).perform(click());
-        onView(withId(R.id.button_para1)).perform(click());
-        onView(withId(R.id.button1)).perform(click());
-        onView(withId(R.id.button_equal)).perform(click());
-        onView(withId(R.id.output)).check(matches(withText("0")));
+        onView(withId(R.id.button1)).perform(click()); //cari id button1 terus di klik
+        onView(withId(R.id.button_para1)).perform(click()); //cari id button_para1 terus di klik
+        onView(withId(R.id.button1)).perform(click()); //cari id button1 terus di klik
+        onView(withId(R.id.button_equal)).perform(click()); //cari id button_equal terus di klik
+        onView(withId(R.id.output)).check(matches(withText("0"))); //menampilkan hasil pengurangan 1% = 0,01
     }
 }
